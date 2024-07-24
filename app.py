2938b70bd4897406e8bb47d1fea863d2a46b975b
inho777 <inho777@onda.co.kr>
2024-07-24 17:33:53 +0900

    fix: app py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
-from handlers.routes import configure_routes
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-configure_routes(app)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
