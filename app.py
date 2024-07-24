dc088e14a59d11f4c37de177902eb74dedd23f33
inho777 <inho777@onda.co.kr>
2024-07-24 16:58:21 +0900

    test: build test

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
+from handlers.routes import configure_routes
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+configure_routes(app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
